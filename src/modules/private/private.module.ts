14a5ea847941ac767dfb8163d9c33c7c2efcb52e
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2024-04-26 19:33:04 -0300

    remove private route

--- src/modules/private/private.module.ts
+++
@@ -1,9 +0,0 @@
-import { Module } from '@nestjs/common';
-import { PrivateController } from './private.controller';
-
-@Module({
-  imports: [],
-  controllers: [PrivateController],
-  providers: [],
-})
-export class PrivateModule {}
