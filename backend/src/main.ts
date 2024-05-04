c3e207cbb43c8b1d354d4e994da918ca4d2e41e2
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2024-05-04 00:02:37 -0300

    add swagger

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,6 +1,7 @@
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 export const PORT = 8080;
 
@@ -16,6 +17,14 @@ async function bootstrap() {
   // Ensures all endpoints are protected from receiving incorrect data.
   // see: https://docs.nestjs.com/techniques/validation#using-the-built-in-validationpipe
   app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+
+  // Swagger config
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Trello Clone API')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(PORT);
 }
