e5a0f676333ed719b862516dd7f085e13c1f4ab4
Ivan Miyashiro <ivo.miyashiro1@gmail.com>
2024-05-17 15:42:41 -0300

    change main faile names to "index.ts"

--- frontend/src/pages/Landing.tsx
+++ frontend/src/pages/Landing.tsx
@@ -1,25 +1,29 @@
 import { Link } from "react-router-dom";
-import { useTheme } from "@/hooks";
+import { useAuth, useTheme } from "@/hooks";
 import { Button, buttonVariants } from "@/components/ui";
 
 const Landing = () => {
   const { toggleTheme } = useTheme();
+  const { user } = useAuth();
 
   return (
     <div className="flex h-screen items-center justify-center gap-10">
       <Button onClick={toggleTheme}>theme</Button>
-      <Link
-        to={"/auth/login"}
-        className={buttonVariants({ variant: "default" })}
-      >
-        Login
-      </Link>
-      <Link
-        to={"/dashboard"}
-        className={buttonVariants({ variant: "default" })}
-      >
-        Dashboard
-      </Link>
+      {user ? (
+        <Link
+          to={"/dashboard"}
+          className={buttonVariants({ variant: "default" })}
+        >
+          Dashboard
+        </Link>
+      ) : (
+        <Link
+          to={"/auth/login"}
+          className={buttonVariants({ variant: "default" })}
+        >
+          Login
+        </Link>
+      )}
     </div>
   );
 };
--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -1,6 +1,6 @@
 import { lazy } from "react";
 
 export const Landing = lazy(() => import("./Landing"));
-export const Login = lazy(() => import("./(auth)/Login/Login"));
-export const Signup = lazy(() => import("./(auth)/Signup/Signup"));
+export const Login = lazy(() => import("./(auth)/Login"));
+export const Signup = lazy(() => import("./(auth)/Signup"));
 export const Dashboard = lazy(() => import("./(dashboard)/Dashboard"));
